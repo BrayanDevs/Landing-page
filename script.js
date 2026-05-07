6095bbb7c8c3a698adbe2504e9d9f2553f39dd58
Bryan Rodriguez <sazukehatake1995@gmail.com>
2026-05-07 11:38:30 -0500

    hola

--- script.js
+++ script.js
@@ -1,60 +1,112 @@
-function actualizarInterfaz(){
-    document.getElementById('visitas').innerText = localStorage.getItem('contadorVisitas') || 0;
-    document.getElementById('Registros').innerText = localStorage.getItem('contadorRegistros') || 0;
-    document.getElementById('Comentarios').innerText = localStorage.getItem('contadorComentarios') || 0;
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyCgYBgHW_eUC_SbeXSBnjURyqOkrxkIAJE",
+  authDomain: "lading-page-metrics.firebaseapp.com",
+  databaseURL: "https://lading-page-metrics-default-rtdb.firebaseio.com",
+  projectId: "lading-page-metrics",
+  storageBucket: "lading-page-metrics.firebasestorage.app",
+  messagingSenderId: "456968293121",
+  appId: "1:456968293121:web:3f27776dce601b7a1c6900",
+  measurementId: "G-5SLD0D9RZQ"
+};
 
-window.onload = function(){
-    let visitas = parseInt(localStorage.getItem('contadorVisitas')) || 0;
-    visitas++;
-    localStorage.setItem('contadorVisitas', visitas);
-    actualizarInterfaz();
-}
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
-const registroForm = document.getElementById('registroForms');
+const visitasRef = database.ref('metricas/visitas');
+const registrosRef = database.ref('metricas/registros');
+const comentariosRef = database.ref('metricas/comentarios');
 
-registrosForm.addEventListener('submit', function(e) {
-    e.preventDefault();
+// ==========================================
+// LÓGICA DE VISITAS (Automática)
+// ==========================================
 
-    let registros = parseInt(localStorage.getItem('contadorRegistros')) || 0;
-    registros++;
-    localStorage.setItem('contadorRegistros', registros);
+// Sumar 1 visita cada vez que se carga la página
 
-    actualizarInterfaz();
-    alert("¡Gracias por registrarte!");
-    registroForm.reset();
+visitasRef.transaction((currentValue) => {
+    return(currentValue || 0 ) + 1;
 });
 
+// Escuchar cambios en el contador de visitas y actualizar el HTML
+visitasRef.on('value', (snapshot) => {
+    document.getElementById('visitas').innerText = snapshot.val() || 0;
+});
+
+// ==========================================
+// LÓGICA DE REGISTROS (Formulario)
+// ==========================================
+
+const registroForms = document.getElementById('registroForms');
+
+registroForms.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    database.ref('metricas/registros').transaction((valorActual) => {
+        return (valorActual || 0) +1;
+    }).then(() => {
+        alert("Gracias por registrarte");
+        registroForms.reset();
+    
+    }).catch((error) => {
+        console.error("Error al actualizar registros: ", error);
+    });    
+});
+
+database.ref('metricas/registros').on('value', (snapshot) => {
+    const totalRegistros = snapshot.val() || 0;
+    const elemenentoContador = document.getElementById('registros');
+    if(elemenentoContador){
+        elemenentoContador.innerText = totalRegistros;
+    }
+});
+
+// ==========================================
+// LÓGICA DE COMENTARIOS (Feedback)
+// ==========================================
 function enviarFeedback(){
-    const cajaTexto = document-getElementById('comentario');
-    const texto = cajaTexto.value.trim();
+    const cajaTexto = document.getElementById('comentario');
+    const mensaje = cajaTexto.value;
 
-    if(texto !== "") {
-        let comentarios = parseInt(localStorage.getItem('contadorComentarios')) || 0;
-        comentarios++;
-        localStorage.setItem('contadorComentaerios', comentarios);
+    const nombreUsuario = document.querySelector('input[name="nombre"]').value || "Anónimo";
+    const correoUsuario = document.querySelector('input[name="email"]').value || "Sin correo";
 
-        actualizarInterfaz();
-        alert("Comentarios enviados, ¡gracias por tu feedback!");
+    if(mensaje.trim() !== ""){
+        const nuevoComentarioRef = database.ref('detalles_comentarios').push();
+        nuevoComentarioRef.set({
+            nombre: nombreUsuario,
+            correo: correoUsuario,
+            comentario: mensaje,
+            fecha: Date.now()
+        });
+
+        database.ref('metricas/comentarios').transaction((valorActual) => {
+            return (valorActual || 0) + 1;
+        });
+
+        alert("Gracias por el comentario");
         cajaTexto.value = "";
     } else {
-        alert("Por Favor, escribe un comentario antes de enviar.");
+        alert("Por favor, escribe un comentario antes de enviar.");
     }
 }
 
-function animarNumero(id, valorFinal){
-    let obj = document.getElementById(id);
-    let inicio = 0;
-    let duracion = 500;
-    let paso = valorFinal / (duracion / 10);
+database.ref('detalles_comentarios').on('value', (snapshot) => {
+    const contenedor = document.getElementById('listaComentarios');
+    contenedor.innerHTML = "";
 
-    let temporizador = setInterval(() =>{
-        inicio += paso;
-        if(inicio >= valorFinal){
-            obj.innerText = valorFinal;
-            clearInterval(temporizador)
-        } else {
-            obj.innerText = Math.floor(inicio)
-        }
-    }, 10);
-}
+    snapshot.forEach((childSnapshot) => {
+        const data = childSnapshot.val();
+        
+    
+        const div = document.createElement('div');
+        div.classList.add('tarjeta-comentario');
+        div.innerHTML = `
+            <div class="user-info">
+                <strong>${data.nombre}</strong> 
+                <span>Usuario: ${data.correo}</span>
+            </div>
+            <p class="text-comment">${data.comentario}</p>
+            <hr>
+        `;
+        contenedor.prepend(div);
+    });
+});
